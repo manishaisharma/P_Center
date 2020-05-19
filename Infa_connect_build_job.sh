73ffb7f2fb52ace4f7237b1a2442758a8149f23a
maanusharma9876 <maanu.sharma9876@gmail.com>
2020-05-19 17:04:36 +0530

    commiting new changes

--- Infa_connect_build_job.sh
+++ Infa_connect_build_job.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
+Jenkins_workspace=/var/lib/jenkins/workspace/Informatica_Start_Pull_Artifcats
+
+SRC_REP=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep SRC_REP | cut -d "=" -f 2`
+DOMAIN=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep DOMAIN | cut -d "=" -f 2`
+INFA_INTEGRATION_SERVICE=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep INFA_INTEGRATION_SERVICE | cut -d "=" -f 2`
+FOLDER=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep FOLDER | cut -d "=" -f 2`
+WORKFLOW=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep WORKFLOW | cut -d "=" -f 2`
+
+
 INFA_WORKDIR=/data/masharma/Jenkins/J_Informatica
 
 LogFileDir=$INFA_WORKDIR/Logs
@@ -7,12 +16,12 @@ date=`date +'%Y-%m-%d%H%M%S'`
 LogFileName=Infa_Jenkins_Login_$date.log
 export USERNAME=$1
 export PASSWORD=$2
-export SRC_REP=$3 
-export DOMAIN=$4
-export INFA_INTEGRATION_SERVICE=$5
-export FOLDER=$6
-export WORKFLOW=$7
-echo $USERNAME $PASSWORD $SRC_REP $DOMAIN $INFA_INTEGRATION_SERVICE $FOLDER $WORKFLOW
+#export SRC_REP=$3 
+#export DOMAIN=$4
+#export INFA_INTEGRATION_SERVICE=$5
+#export FOLDER=$6
+#export WORKFLOW=$7
+echo $SRC_REP $DOMAIN $INFA_INTEGRATION_SERVICE $FOLDER $WORKFLOW
 
 cat /dev/null>$LogFileDir/$LogFileName
 cd $INFA_WORKDIR
@@ -21,16 +30,16 @@ pmrep connect -r $SRC_REP -d $DOMAIN -n $USERNAME -x $PASSWORD >>$LogFileDir/$Lo
 RETURN_CODE=$?
 if [ $RETURN_CODE == 0 ]
 then 
-echo " Connected to the Repository INFA_REPO, check logs at /data/masharma/Jenkins "
-pmcmd startworkflow -sv $INFA_INTEGRATION_SERVICE -d $DOMAIN -u $USERNAME -p $PASSWORD -f $FOLDER -wait $WORKFLOW
-if [ $? == 0 ]
-then 
-echo "Job executed, check logs at $LogFileDir/$LogFileName "
+	echo " Connected to the Repository INFA_REPO, check logs at /data/masharma/Jenkins "
+	pmcmd startworkflow -sv $INFA_INTEGRATION_SERVICE -d $DOMAIN -u $USERNAME -p $PASSWORD -f $FOLDER -wait $WORKFLOW
+			if [ $? == 0 ]
+			then 
+				echo "Job executed, check logs at $LogFileDir/$LogFileName "
+			else
+				echo "Failed to EXECUTE job,  check logs at $LogFileDir/$LogFileName"
+				exit 1
+            fi
 else
-echo "Failed to EXECUTE job,  check logs at $LogFileDir/$LogFileName"
+   	echo "Failed to Connect to  Repository INFA_REPO,  check logs at $LogFileDir/$LogFileName"
    	exit 1
-            	fi
-        	else
-        	echo "Failed to Connect to  Repository INFA_REPO,  check logs at $LogFileDir/$LogFileName"
-        	exit 1
-        	fi
+fi
