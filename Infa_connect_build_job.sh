3e48cc45b2286c65f7e19501d9084c7cab1c06ec
maanusharma9876 <maanu.sharma9876@gmail.com>
2020-05-26 14:54:47 +0530

    Update Infa_connect_build_job.sh

--- Infa_connect_build_job.sh
+++ Infa_connect_build_job.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
+export USERNAME=$1
+export PASSWORD=$2
+export WORKSPACE=$3
 
-Jenkins_workspace=/var/lib/jenkins/workspace/Informatica_Start_Pull_Artifcats
+Jenkins_workspace=$WORKSPACE
+
 
 SRC_REP=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep SRC_REP | cut -d "=" -f 2`
 DOMAIN=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep DOMAIN | cut -d "=" -f 2`
@@ -8,14 +12,14 @@ INFA_INTEGRATION_SERVICE=`cat $Jenkins_workspace/Informatica_Job_config.prm | gr
 FOLDER=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep FOLDER | cut -d "=" -f 2`
 WORKFLOW=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep WORKFLOW | cut -d "=" -f 2`
 INFA_WORKDIR=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep INFA_WORKDIR | cut -d "=" -f 2`
+BASH_PROFILE_FILE=`cat $Jenkins_workspace/Informatica_Job_config.prm | grep BASH_PROFILE_FILE | cut -d "=" -f 2`
 
 
 
 LogFileDir=$INFA_WORKDIR/Logs
 date=`date +'%Y-%m-%d%H%M%S'`
 LogFileName=Infa_Jenkins_Login_$date.log
-export USERNAME=$1
-export PASSWORD=$2
+
 #export SRC_REP=$3 
 #export DOMAIN=$4
 #export INFA_INTEGRATION_SERVICE=$5
@@ -25,7 +29,7 @@ echo $SRC_REP $DOMAIN $INFA_INTEGRATION_SERVICE $FOLDER $WORKFLOW
 
 cat /dev/null>$LogFileDir/$LogFileName
 cd $INFA_WORKDIR
-. /data/masharma/Jenkins/J_Informatica/.bash_profile 
+. $BASH_PROFILE_FILE
 pmrep connect -r $SRC_REP -d $DOMAIN -n $USERNAME -x $PASSWORD >>$LogFileDir/$LogFileName
 RETURN_CODE=$?
 if [ $RETURN_CODE == 0 ]
