5d17d68980447e65e51d1b5d2491e9bb29a008a6
maanusharma9876 <maanu.sharma9876@gmail.com>
2020-05-27 18:31:28 +0530

    Update Informatica_Repository_watch.py

--- J_Informatica_Scripts/Informatica_Repository_watch.py
+++ J_Informatica_Scripts/Informatica_Repository_watch.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 20 15:45:58 2020
-
-@author: msharm77
-"""
 
 
 import cx_Oracle
@@ -21,7 +15,7 @@ def  repo_watch():
     objects_changed=0
     LOCATION = "/usr/lib/oracle/19.3/client64/lib"
     src_files=""   
-    REPO_QUERY_LOCATION="/var/lib/jenkins/workspace/RepoWatch/J_Informatica_Scripts"
+    REPO_QUERY_LOCATION="/var/lib/jenkins/workspace/RepoWatch/SQL_Scripts/Watch_Repository_every_minute.txt"
     db_credentials_filename="/data/masharma/Jenkins/J_Informatica/creds_repo.prm"
     creds = open(db_credentials_filename, 'r')
     lines = creds.readline()
@@ -31,21 +25,22 @@ def  repo_watch():
     host= columns[2]
     SID=columns[3]
     connection_string = user+'/'+password +'''@(DESCRIPTION=
-        (ADDRESS_LIST=
-    (ADDRESS=
-        (PROTOCOL=TCP)
-        (HOST=''' +host+''')
-        (PORT=1521)
-    )
-        )
-        (CONNECT_DATA=
-    (SID='''+SID+''')
-        )
-    )'''
+                                                            (ADDRESS_LIST=
+                                                                (ADDRESS=
+                                                                    (PROTOCOL=TCP)
+                                                                    (HOST=''' +host+''')
+                                                                    (PORT=1521)
+                                                                )
+                                                            )
+                                                            (CONNECT_DATA=
+                                                                (SID='''+SID+''')
+                                                            )
+                                                        )'''
     connection = cx_Oracle.connect(connection_string)
     cursor = connection.cursor()
     with open(REPO_QUERY_LOCATION,'r') as SQL:    
-        result=cursor.execute(SQL)
+        for statement in SQL:
+           result=cursor.execute(statement)
 
     for row in result:
         objects_changed=row[0]
@@ -53,5 +48,9 @@ def  repo_watch():
         
     cursor.close()
     connection.close()
+    assert(objects_changed)
     return(objects_changed)
 
+if __name__ == "__main__":
+    print(repo_watch())
+    #repo_watch()
