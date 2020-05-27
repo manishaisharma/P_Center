2fdff0cbabe7ba02ab2229876317711143782c98
maanusharma9876 <maanu.sharma9876@gmail.com>
2020-05-27 17:54:30 +0530

    Update Informatica_Repository_watch.py

--- J_Informatica_Scripts/Informatica_Repository_watch.py
+++ J_Informatica_Scripts/Informatica_Repository_watch.py
@@ -15,48 +15,43 @@ import pandas as pd
 import sys
 import nose
 import coverage
-#python -m unittest test1.TestStringMethods.test_connection
-#python -m unittest -v test1
 
-
-
-        
-def repo_scan():
+def  repo_watch():
       
-        objects_changed=0
-        LOCATION = "/usr/lib/oracle/19.3/client64/lib"
-        src_files=""   
-        REPO_QUERY_LOCATION=config['myreposcan']['sql']
-        db_credentials_filename="/data/masharma/Jenkins/J_Informatica/creds_repo.prm"
-        creds = open(db_credentials_filename, 'r')
-        lines = creds.readline()
-        columns = lines.split(" ")
-        user= columns[0]
-        password=columns[1]    
-        host= columns[2]
-        SID=columns[3]
-        connection_string = user+'/'+password +'''@(DESCRIPTION=
-                                                            (ADDRESS_LIST=
-                                                                (ADDRESS=
-                                                                    (PROTOCOL=TCP)
-                                                                    (HOST=''' +host+''')
-                                                                    (PORT=1521)
-                                                                )
-                                                            )
-                                                            (CONNECT_DATA=
-                                                                (SID='''+SID+''')
-                                                            )
-                                                        )'''
-        connection = cx_Oracle.connect(connection_string)
-        cursor = connection.cursor()
-        with open(REPO_QUERY_LOCATION,'r') as SQL:            
-                result=cursor.execute(SQL)
+    objects_changed=0
+    LOCATION = "/usr/lib/oracle/19.3/client64/lib"
+    src_files=""   
+    REPO_QUERY_LOCATION="/var/lib/jenkins/workspace/RepoWatch/J_Informatica_Scripts"
+    db_credentials_filename="/data/masharma/Jenkins/J_Informatica/creds_repo.prm"
+    creds = open(db_credentials_filename, 'r')
+    lines = creds.readline()
+    columns = lines.split(" ")
+    user= columns[0]
+    password=columns[1]    
+    host= columns[2]
+    SID=columns[3]
+    connection_string = user+'/'+password +'''@(DESCRIPTION=
+        (ADDRESS_LIST=
+    (ADDRESS=
+        (PROTOCOL=TCP)
+        (HOST=''' +host+''')
+        (PORT=1521)
+    )
+        )
+        (CONNECT_DATA=
+    (SID='''+SID+''')
+        )
+    )'''
+    connection = cx_Oracle.connect(connection_string)
+    cursor = connection.cursor()
+    with open(REPO_QUERY_LOCATION,'r') as SQL:    
+        result=cursor.execute(SQL)
 
-        for row in result:
-            objects_changed=row[0]
-           
-            
-        cursor.close()
-        connection.close()
-        assert(objects_changed)
+    for row in result:
+        objects_changed=row[0]
+       
         
+    cursor.close()
+    connection.close()
+    return(objects_changed)
+
