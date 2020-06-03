98e549d7d5e94d1d534a9788a1aa83b88f2382e3
maanusharma9876 <maanu.sharma9876@gmail.com>
2020-06-03 11:49:04 +0530

    Update wf_m_client_complaint_FACT_test.py

--- J_Unit_Testing/wf_m_client_complaint_FACT_test.py
+++ J_Unit_Testing/wf_m_client_complaint_FACT_test.py
@@ -98,7 +98,7 @@ def prepare_source_data():
         agg2=df_inner.groupby(["RAND_CLIENT", "Date_received"])["Complaint_ID"].count().reset_index(name='counts')
         agg2=agg2.loc[agg.values==True]
         aggsrcdata = agg2  #[agg2.counts >2]
-        agg2.to_csv('/data/masharma/dd.csv')
+        agg2.to_csv('/data/masharma/dd.csv', index=False)
        
         
         
